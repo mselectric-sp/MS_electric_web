14b414f9f19dd7aa6bc081ad0fc32115cc4ca8ef
M&S_Electric <ms.electric.homesite@gmail.com>
2026-04-13 18:51:54 +0200

    P00- Stay in website when compliting form + local time for email form

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ if(localStorage.getItem("cookiesAceptadas")){
     document.getElementById("cookies").style.display="none"
     
 }
+
+const form = document.getElementById("formulario");
+const mensaje = document.getElementById("mensaje-exito");
+
+form.addEventListener("submit", async function(e){
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // 👉 Añadir hora local de España
+  const ahora = new Date().toLocaleString("es-ES", {
+    timeZone: "Europe/Madrid"
+  });
+  data.append("hora_local", ahora);
+
+  const response = await fetch("https://formspree.io/f/mojyapoa", {
+    method: "POST",
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  if(response.ok){
+    form.reset();
+    mensaje.style.display = "block";
+  } else {
+    alert("Hubo un error, inténtalo de nuevo");
+  }
+  form.style.display = "none";
+});
